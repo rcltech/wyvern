016276dc998c97e7a11c1b73924e0e2238373d8a
seanchok <seanchok@gmail.com>
2019-09-19 16:50:45 +0800

    write led button control with servo

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -1,36 +1,56 @@
 #include <Servo.h>
- 
-int servoPin = 7;
-int ledPin = 8;
-int buttonPin = 9;
 
 Servo servo;  
- 
-int servoAngle = 0;   // servo position in degrees
-int ledState = 0;
 
+const int servoPin = 7;
+const int ledPin = 8; // SIG1
+const int buttonPin = 9; // SIG2
+
+int ledState = LOW; // current state of output pin
+int buttonState; // current reading from input pin
+int lastButtonState = HIGH; // previous reading from input pin, defaults to HIGH at initialisation
+int servoAngle = 90;   // servo position in degrees, set to middle
+
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;
 
 void setup()
 {
   Serial.begin(9600);
   servo.attach(servoPin);
   pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  digitalWrite(ledPin, ledState);
 }
- 
  
 void loop()
 {
-  servo.write(45);
-  if (ledState == 0) {
-    digitalWrite(ledPin, HIGH);
-    ledState = 1;
+  int reading = digitalRead(buttonPin);
+
+  // if the switch changed, due to noise or pressing:
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
   }
-  delay(1000);
-  
-  servo.write(90);
-  if (ledState == 1) {
-    digitalWrite(ledPin, LOW);
-    ledState = 0;
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    buttonState = reading;
+
+    if (buttonState == LOW) {
+      ledState = !ledState;
+      delay(200);
+    }
   }
-  delay(1000);
+
+  // set the LED
+  digitalWrite(ledPin, ledState);
+
+  // set the servo
+  if (ledState == HIGH) {
+    servo.write(30);
+  } else {
+    servo.write(150);
+  }
+
+  lastButtonState = reading;
 }
