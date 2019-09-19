6347188db448bffcfb7b114a02498e79904ba8b4
seanchok <seanchok@gmail.com>
2019-09-19 16:50:24 +0800

    create simple led control program for grove led button module

---
+++ arduino/ledControl/ledControl.ino
@@ -0,0 +1,67 @@
+const int ledPin = 8; // SIG1
+const int buttonPin = 9; // SIG2
+const boolean breathMode = true;  // if or not the led lights as breath mode when it's on
+
+int ledState = LOW;         // the current state of the output pin
+int ledFadeValue = 0;
+int ledFadeStep = 5;
+int ledFadeInterval = 20;   //milliseconds
+int buttonState;             // the current reading from the input pin
+int lastButtonState = HIGH;   // the previous reading from the input pin
+
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+unsigned long lastLedFadeTime = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  digitalWrite(ledPin, ledState);
+}
+
+void loop() {
+  int reading = digitalRead(buttonPin);
+
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer
+    // than the debounce delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      if (buttonState == LOW) {  //button is pressed
+        ledState = !ledState;
+        ledFadeValue = 0;
+        lastLedFadeTime = millis();
+      }
+    }
+  }
+
+  // set the LED:
+  if (breathMode && ledState != LOW) {
+    if (millis() - lastLedFadeTime > ledFadeInterval) {
+      lastLedFadeTime = millis();
+      analogWrite(ledPin, ledFadeValue);
+      ledFadeValue += ledFadeStep;
+      if (ledFadeValue > 255){
+        ledFadeValue = 255 - ledFadeStep;
+        ledFadeStep = -ledFadeStep;
+      } else if (ledFadeValue < 0) {
+        ledFadeValue = 0;
+        ledFadeStep = -ledFadeStep;
+      }
+    }
+  } else {
+    digitalWrite(ledPin, ledState);
+  }
+
+  lastButtonState = reading;
+}
