37742c0f51508ad6c9d340e0d333cfb513f1889c
Sean Chok <seanchok@gmail.com>
2019-09-21 23:37:43 +0800

    create cycle behavior to control servo in main program (#3)

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -9,10 +9,18 @@ const int buttonPin = 9; // SIG2
 int ledState = LOW; // current state of output pin
 int buttonState; // current reading from input pin
 int lastButtonState = HIGH; // previous reading from input pin, defaults to HIGH at initialisation
-int servoAngle = 90;   // servo position in degrees, set to middle
 
 unsigned long lastDebounceTime = 0;
 unsigned long debounceDelay = 50;
+
+bool cycle = false;
+const int period = 1000 * 2;
+unsigned long lastMovementTime;
+unsigned long currentTime;
+bool airconSwitch = false;
+
+const int servoAngleForOn = 30;
+const int servoAngleForOff = 150;
 
 void setup()
 {
@@ -21,6 +29,7 @@ void setup()
   pinMode(ledPin, OUTPUT);
   pinMode(buttonPin, INPUT);
   digitalWrite(ledPin, ledState);
+  servo.write(servoAngleForOff);
 }
  
 void loop()
@@ -36,8 +45,17 @@ void loop()
   if ((millis() - lastDebounceTime) > debounceDelay) {
     buttonState = reading;
 
-    if (buttonState == LOW) {
+    if (buttonState == LOW) { // when button is pressed
+      cycle = !cycle;
       ledState = !ledState;
+      lastMovementTime = millis();
+      if (cycle) {
+        servo.write(servoAngleForOn);
+        airconSwitch = true;
+      } else {
+        servo.write(servoAngleForOff);
+        airconSwitch = false;
+      }
       delay(200);
     }
   }
@@ -46,10 +64,17 @@ void loop()
   digitalWrite(ledPin, ledState);
 
   // set the servo
-  if (ledState == HIGH) {
-    servo.write(30);
-  } else {
-    servo.write(150);
+  if (cycle) {
+    currentTime = millis();
+    if (currentTime - lastMovementTime > period) {
+      if (airconSwitch) {
+        servo.write(servoAngleForOff);
+      } else {
+        servo.write(servoAngleForOn);
+      }
+      airconSwitch = !airconSwitch;
+      lastMovementTime = currentTime;
+    }
   }
 
   lastButtonState = reading;
