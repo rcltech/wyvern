401908ac746bccc463e5c46f2ee858130e403eb8
seanchok <seanchok@gmail.com>
2019-09-25 01:06:40 +0800

    add 2nd servo and control both servos simultaneously
    #4

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -1,8 +1,10 @@
 #include <Servo.h>
 
-Servo servo;  
+Servo servo1;
+Servo servo2;  
 
-const int servoPin = 7;
+const int servo1Pin = 6;
+const int servo2Pin = 7;
 const int ledPin = 8; // SIG1
 const int buttonPin = 9; // SIG2
 
@@ -10,26 +12,42 @@ int ledState = LOW; // current state of output pin
 int buttonState; // current reading from input pin
 int lastButtonState = HIGH; // previous reading from input pin, defaults to HIGH at initialisation
 
-unsigned long lastDebounceTime = 0;
+unsigned long lastDebounceTime;
 unsigned long debounceDelay = 50;
 
 bool cycle = false;
 const int period = 1000 * 2;
-unsigned long lastMovementTime;
+unsigned long lastMovementTime = 0;
 unsigned long currentTime;
 bool airconSwitch = false;
 
 const int servoAngleForOn = 30;
 const int servoAngleForOff = 150;
 
+void moveServos(int startAngle, int endAngle) {
+  if (endAngle > startAngle) {
+    for (int i = startAngle + 1; i <= endAngle; i+=1) {
+      servo1.write(i);
+      servo2.write(i);
+    }
+  } else {
+    for (int i = startAngle - 1; i >= endAngle; i-=1) {
+      servo1.write(i);
+      servo2.write(i);
+    }
+  }
+}
+
 void setup()
 {
   Serial.begin(9600);
-  servo.attach(servoPin);
+  servo1.attach(servo1Pin);
+  servo2.attach(servo2Pin);
   pinMode(ledPin, OUTPUT);
   pinMode(buttonPin, INPUT);
   digitalWrite(ledPin, ledState);
-  servo.write(servoAngleForOff);
+  delay(1000);
+  moveServos(90, servoAngleForOff);//
 }
  
 void loop()
@@ -50,10 +68,10 @@ void loop()
       ledState = !ledState;
       lastMovementTime = millis();
       if (cycle) {
-        servo.write(servoAngleForOn);
+        moveServos(servoAngleForOff, servoAngleForOn);
         airconSwitch = true;
       } else {
-        servo.write(servoAngleForOff);
+        moveServos(servoAngleForOn, servoAngleForOff);
         airconSwitch = false;
       }
       delay(200);
@@ -63,18 +81,19 @@ void loop()
   // set the LED
   digitalWrite(ledPin, ledState);
 
-  // set the servo
+  // set the servos
   if (cycle) {
     currentTime = millis();
     if (currentTime - lastMovementTime > period) {
       if (airconSwitch) {
-        servo.write(servoAngleForOff);
+        moveServos(servoAngleForOn, servoAngleForOff);
       } else {
-        servo.write(servoAngleForOn);
+        moveServos(servoAngleForOff, servoAngleForOn);
       }
       airconSwitch = !airconSwitch;
       lastMovementTime = currentTime;
     }
+    delay(25);
   }
 
   lastButtonState = reading;
